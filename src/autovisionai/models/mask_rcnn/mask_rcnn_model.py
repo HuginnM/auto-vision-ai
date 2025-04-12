46880fcb528283d09d3401dc6d3fec59f59a9573
Arthur Sobol <arthur.sobol.ai@gmail.com>
2025-04-11 20:39:10 -0700

    Developed create_model function to get pretrained MaskRCNN model. Written unit tests for the model. Created config for MaskRCNN.

---
+++ src/autovisionai/models/mask_rcnn/mask_rcnn_model.py
@@ -0,0 +1,32 @@
+import torchvision
+from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor, MaskRCNN_ResNet50_FPN_Weights
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+
+from autovisionai.configs.config import CONFIG
+
+
+def create_model(n_classes: int, pretrained: bool = True) -> MaskRCNNPredictor:
+    """
+    Creates the Mask R-CNN model based on `maskrcnn_resnet50_fpn`.
+
+    :param n_classes: number of classes the model should predict.
+    :param pretrained: an indicator to load pretrained model or not.
+    :return: MaskRCNNPredictor.
+    """
+    # Loading an instance of segmentation model
+    weights = MaskRCNN_ResNet50_FPN_Weights.DEFAULT if pretrained else None
+    model = torchvision.models.detection.maskrcnn_resnet50_fpn(weights=weights)
+
+    # Getting the number of input features for the classifier
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    # Replacing the pre-trained head with a new one.
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_channels=in_features, num_classes=n_classes)
+
+    # Getting the number of input features for the mask classifier
+    in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
+    hidden_layer = CONFIG['mask_rcnn']['model']['hidden_size'].get()
+
+    model.roi_heads.mask_predictor = MaskRCNNPredictor(
+        in_channels=in_features_mask, dim_reduced=hidden_layer, num_classes=n_classes)
+
+    return model
---
+++ tests/models/mask_rcnn/mask_rcnn_model_test.py
@@ -0,0 +1,33 @@
+import torch
+from numpy.testing import assert_almost_equal
+
+from autovisionai.models.mask_rcnn.mask_rcnn_model import *
+
+
+def set_seed(seed):
+    torch.manual_seed(seed)
+
+
+def test_create_model():
+    set_seed(42)
+    test_input = torch.randn((1, 3, 128, 256))
+
+    mask_rcnn = create_model(2)
+
+    assert str(mask_rcnn.roi_heads.mask_predictor).replace(" ", "") == '''MaskRCNNPredictor(
+                    (conv5_mask): ConvTranspose2d(256, 256, kernel_size=(2, 2), stride=(2, 2))
+                    (relu): ReLU(inplace=True)
+                    (mask_fcn_logits): Conv2d(256, 2, kernel_size=(1, 1), stride=(1, 1))
+                    )'''.replace(" ", "")
+
+    assert str(mask_rcnn.roi_heads.box_predictor).replace(" ", "") == '''FastRCNNPredictor(
+                            (cls_score): Linear(in_features=1024, out_features=2, bias=True)
+                            (bbox_pred): Linear(in_features=1024, out_features=8, bias=True)
+                            )'''.replace(" ", "")
+
+    mask_rcnn.eval()
+    output = mask_rcnn.forward(test_input)
+    assert_almost_equal(output[0]['boxes'].mean().item(), 101.84027099609375, decimal=5)
+
+    params_mask_rcnn = sum(p.numel() for p in mask_rcnn.parameters() if p.requires_grad)
+    assert params_mask_rcnn == 43699995
