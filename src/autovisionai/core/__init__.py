8008b0c34668550947e01de9e34ed605ff84dc66
Arthur Sobol <arthur.sobol.ai@gmail.com>
2025-05-10 16:32:12 -0700

    chore: move __init__.py from core to to src/autovisionai

--- src/autovisionai/core/__init__.py
+++
@@ -1,6 +0,0 @@
-from autovisionai.core.configs import CONFIG, PROJECT_VERSION
-from autovisionai.core.loggers import setup_app_logger
-
-__version__ = PROJECT_VERSION
-
-setup_app_logger(CONFIG.logging.app_logger)
